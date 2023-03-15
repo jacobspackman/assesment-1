8d37fd89183cd87b39ad4915a486659e272590ba
Jacob Spackman <jacobts202@gmail.com>
2023-03-14 21:30:57 -0700

    sdlc portion completed

--- assessment-js-basics-1/sdlc.js
+++ assessment-js-basics-1/sdlc.js
@@ -18,3 +18,13 @@
     The first one is done for you. 
 */
 const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+
+const analysistOfRequirment = "This is the second step, This is where the developers analyse the requirments of the project while keeping the code of the software and desgin in mind"
+
+const design = "This is the third step, This is where the developer chooses how the project eill be completed, such as which language they will be using to code the project."
+
+const implementation = "This is the fourth step, This is where the developer will actually code! This step also includeds debugging, and is usually the longest step in sdlc "
+
+const testingAndIntergration = "This is the fifth step, once the code is written and debugged it needs to be tested in a safe enviorment to make sure this new code doesn't contredict the existing code. Once this has been tested the new code will then be deplyed and intergraded into the existing software."
+
+const maintain = "This is the sixth step, once the new code is deployed it needs to be maintained, with users changing and new bugs being discovered, the code needs updated form time to time to vreate the best user experience."
