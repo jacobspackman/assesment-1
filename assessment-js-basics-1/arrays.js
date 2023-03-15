4f4894ebbcfddc9c3bdccb1f7d6e06d93fa67811
Jacob Spackman <jacobts202@gmail.com>
2023-03-14 21:50:11 -0700

    arrays finished

--- assessment-js-basics-1/arrays.js
+++ assessment-js-basics-1/arrays.js
@@ -3,12 +3,14 @@
 
 const faveColors = ['red', 'green', 'black']
 
-//CODE HERE
+const colorCopy = faveColors.slice(0,3)
+
 
 //////////////////PROBLEM 2////////////////////
 /* Add a fourth color to the end of the 'colorCopy' array using the push method. */
 
-//CODE HERE
+colorCopy.push("blue");
+
 
 //////////////////PROBLEM 3////////////////////
 /*
@@ -18,7 +20,7 @@ const faveColors = ['red', 'green', 'black']
 
 const numbers = [1, 2, 3, 4, 5]
 
-//CODE HERE
+const middleNums = numbers.slice(1, 4);
 
 
 //////////////////PROBLEM 4////////////////////
@@ -33,4 +35,12 @@ const numbers = [1, 2, 3, 4, 5]
 
 const bigOrSmallArray = [1, 99, 42, 69, 102, -10, 159, 352]
 
-// CODE HERE
+const answers = []
+
+for (let i = 0; i < bigOrSmallArray.length; i++){
+  if(bigOrSmallArray[i] <= 100){
+    answers.push("small")
+  }else {
+    answers.push("big")
+  }
+}
