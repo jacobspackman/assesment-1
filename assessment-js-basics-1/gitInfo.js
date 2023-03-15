903096dfe4d02c9762832e38243f86949978d2b8
Jacob Spackman <jacobts202@gmail.com>
2023-03-14 22:04:42 -0700

    gitinfo finished

--- assessment-js-basics-1/gitInfo.js
+++ assessment-js-basics-1/gitInfo.js
@@ -9,7 +9,7 @@
     It should be a string containing your best definition of what Git is.
 */
 
-//CODE HERE
+let gitDefinition = "git is a software that lets you save snapshots of your project. This allows you look back on iterations of your code and even lode up that version of your code if you needed to."
 
 //////////////////PROBLEM 2////////////////////
 /*
@@ -17,7 +17,7 @@
     It should be a string containing your best definition of what GitHub is.
 */
 
-//CODE HERE
+let gitHubDefinition = "gitHub is a website that you can send your git commits to. This website lets you veiw your git entries for each project in `depoitories`. This also lets you share your code with coworkers or anyone you are working with on that project."
 
 //////////////////PROBLEM 3////////////////////
 /*
@@ -25,7 +25,7 @@
     It should be a string containing your best definition of what 'git init' does.
 */
 
-//CODE HERE
+let gitInitDefinition = "git init basically makes the git. You need to vreate the place you are saving your project to before you can actually save it. git init does this. also init is short hand for initialize. So you are initializing your git"
 
 //////////////////PROBLEM 4////////////////////
 /*
@@ -33,7 +33,7 @@
     It should be a string containing your best definition of what 'git clone' does.
 */
 
-//CODE HERE
+let gitCloneDefinition = "Git clone, clones your git. this is so the same code/projectg can be saved into another git."
 
 //////////////////PROBLEM 5////////////////////
 /*
@@ -41,7 +41,7 @@
     It should be a string containing your best definition of what 'git status' does.
 */
 
-//CODE HERE
+let gitStatusDefinition = "Git status shows you the status of your git"
 
 //////////////////PROBLEM 6////////////////////
 /*
@@ -52,7 +52,9 @@
     It should be a string containing the code to add all files.
 */
 
-//CODE HERE
+let gitAddDefinition = "Git add stages all your code to be saved. esentially saying this is what we are going to save, but not actually sayving it"
+
+let gitAddCode = "git add ."
 
 //////////////////PROBLEM 7////////////////////
 /*
@@ -63,7 +65,9 @@
     It should be a string containing the code to commit using the message "initial commit".
 */
 
-//CODE HERE
+let gitCommitDefinition = "git commit is the actual act of saving your code to the git. This is also where you would name the save, asially you name the save/commit something discriptive."
+
+let gitCommitCode = `git commit -m "initial commit"`
 
 
 //////////////////PROBLEM 8////////////////////
@@ -72,4 +76,4 @@
     It should be a string containing your best definition of what 'git push' does.
 */
 
-//CODE HERE
+let gitPushDefinition = "git push is the command to push you git to github. git commit saves your code to local git and git push pushes that local git to the website github"
